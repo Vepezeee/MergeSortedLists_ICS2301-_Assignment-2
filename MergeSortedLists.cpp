55246a9f57fdcd63e7d96c0be67f0bb681c1861a
Vepezeee <164472129+Vepezeee@users.noreply.github.com>
2025-01-21 22:14:57 +0300

    Add files via upload
    Group Members 
    SCT221-0730/2022 Benson Maingi 
    SCT221-0282/2022 Bonface Gekara

---
+++ MergeSortedLists.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void mergeSortedLists(int list1[], int size1, int list2[], int size2, int mergedList[]) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < size1 && j < size2) {
+        if (list1[i] <= list2[j]) {
+            mergedList[k++] = list1[i++];
+        } else {
+            mergedList[k++] = list2[j++];
+        }
+    }
+
+    while (i < size1) {
+        mergedList[k++] = list1[i++];
+    }
+
+    while (j < size2) {
+        mergedList[k++] = list2[j++];
+    }
+}
+
+int main() {
+    int list1[] = {11, 13, 17};
+    int list2[] = {2, 4, 6, 8, 10};
+    int size1 = sizeof(list1) / sizeof(list1[0]);
+    int size2 = sizeof(list2) / sizeof(list2[0]);
+    int mergedList[size1 + size2];
+
+    mergeSortedLists(list1, size1, list2, size2, mergedList);
+
+    printf("Merged Sorted List: ");
+    for (int i = 0; i < size1 + size2; i++) {
+        printf("%d ", mergedList[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
